b0e6ca581d9c8b676fa625e2ffbc886d465d21f5
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-09-15 00:46:46 -0500

    Update example to use lena image

--- example/pgm_example.rb
+++ example/pgm_example.rb
@@ -2,10 +2,10 @@ require 'pgm'
 
 pgm = PGM::Bindings::Structs::Pgm.new
 
-PGM::Functions.load_pgm(pgm, './Feep.pgm')
+PGM::Functions.load_pgm(pgm, './lena.pgm')
 
 pgm_out = PGM::Bindings::Structs::Pgm.new
 
 PGM::Functions.invert_colors(pgm, pgm_out)
 
-PGM::Functions.save_pgm(pgm_out, './feep_out.pgm')
+PGM::Functions.save_pgm(pgm_out, './out.pgm')
